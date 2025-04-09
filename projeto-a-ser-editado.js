b07f786954c8cc755532fb370a8852de62d8bb68
patrickmqs <150060111+patrickmqs@users.noreply.github.com>
2025-04-09 16:42:04 -0300

    Update (função carrinho) projeto-a-ser-editado.js
    a função aciona quando o botão adicionarAoPedido e clicado
    
    o let tabela pega o corpo de onde os itens serão adicionados 
    
    o let linha cria uma nova linha na tabela 
    
    o linha.innerHTMl coloca o conteúdo na linha que foi criada 
    
    essa parte (`<td>${produto}</td><td>R$ ${preço}</td>`) formata o produto na linha
    
    o <td> cria uma célula na tabela e o ${produto} e o ${preço} são substituídos pelos valores de cada item adicionado

--- projeto-a-ser-editado.js
+++ projeto-a-ser-editado.js
@@ -1,1 +1,5 @@
-
+function adicionarAoPedido(produto, preco) {
+    let tabela = document.getElementById("carrinho").getElementsByTagName("tbody")[0];
+    let linha = tabela.insertRow();
+    linha.innerHTML = `<td>${produto}</td><td>R$ ${preco},00</td>`;
+  }
