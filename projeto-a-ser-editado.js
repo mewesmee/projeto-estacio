8c09e2ce3b079de2304b2e576a3fadbbb1547c0a
patrickmqs <150060111+patrickmqs@users.noreply.github.com>
2025-04-09 21:45:30 -0300

    Update projeto-a-ser-editado.js

--- projeto-a-ser-editado.js
+++ projeto-a-ser-editado.js
@@ -1,5 +1,17 @@
 function adicionarAoPedido(produto, preco) {
-    let tabela = document.getElementById("carrinho").getElementsByTagName("tbody")[0];
-    let linha = tabela.insertRow();
-    linha.innerHTML = `<td>${produto}</td><td>R$ ${preco},00</td>`;
-  }
+  let carrinho = document.getElementById("carrinho").getElementsByTagName("tbody")[0]; 
+  let linha = carrinho.insertRow();
+  linha.innerHTML = `
+    <td>${produto}</td>
+    <td>R$ ${preco.toFixed(2).replace('.', ',')}</td>
+    <td><input type="number" value="1" min="1" class="quantidade"></td>
+    <td><button class="remover" onclick="this.parentNode.parentNode.remove()">Remover</button></td>`;
+}
+
+function limparPedido() {
+  document.getElementById("carrinho").getElementsByTagName("tbody")[0].innerHTML = ""; 
+}
+
+function finalizarPedido(){
+    
+}
